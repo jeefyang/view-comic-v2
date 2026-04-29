c705e05b5e2cc3e799b67969313a68b5f20ead24
jeefyang <jeefyang@sina.com>
2026-04-29 16:18:11 +0800

    去掉错误提示

--- common/apis/tools/transFetch.ts
+++ common/apis/tools/transFetch.ts
@@ -57,6 +57,7 @@ export class TransFetch<T extends ReturnType<typeof apiUrlsTrans>> {
             }
             let url = this.prevUrl + item.url;
             if (item.method == "GET" && data) {
+                //@ts-expect-error
                 url += "?" + new URLSearchParams(data).toString();
             }
             const headers = await this.getHeaderFn(key);
